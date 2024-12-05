66fe326a502fb109b18484f9f58919680a4effdd
Edvaldo-jb-Junior <juniorb2325@gmail.com>
2024-12-05 14:44:08 -0300

    projeto-pilates

---
+++ scripts.js
@@ -0,0 +1,24 @@
+
+const div = document.querySelector(".div-pilate")
+const forma = document.querySelector(".formacao")
+
+
+function pilates() {
+    div.style.visibility = "visible"
+    div.style.transform = "translateX(0)"
+}
+ function esconderPilates() {
+    div.style.visibility = "hidden"
+    div.style.transform = "translateX(0)"
+ }
+
+ function formacao(){
+    forma.style.visibility = "visible"
+    forma.style.transform = "translateX(0)"
+ }
+
+ function esconderformacao() {
+    forma.style.visibility = "hidden"
+    forma.style.transform = "translatex(0)"
+ }
+
